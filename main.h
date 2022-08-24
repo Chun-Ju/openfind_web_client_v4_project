59c7b92e2c0b59c33acefde4b613086ff756dcba
Chun-Ju <47974211+Chun-Ju@users.noreply.github.com>
2022-08-24 06:39:25 -0400

    createDir時如果dir/file已存在的清空或移除處理
    問題(1)createDir時如果錯誤回傳是EEXIST,會直接當錯誤處理,但其實稍作處理便可執行
    修正(1)createDir(char *dir)中的dir爲路徑,用opendir開啟後成功即為資料夾,就把其下檔案全刪除
           並回傳成功,如果opendir失敗且errno == ENOTDIR則視其為檔案,則將末端的/皆去除,並刪除該檔,
           再呼叫createDir建立該資料夾
           [!]刪除資料夾的部分目前侷限在刪除包含多檔的資料夾,但若有需求,可先判斷是否是資料夾然後
              recursive的去刪除
    define修正
    新增(1)ERR_DOPEN表錯誤(開啟資料夾)
    刪除(1)PATH_MAX(limit.h已經有定義,用途一樣則沿用)

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <dirent.h>
 
 #ifndef _STDIO_H_
 #define _STDIO_H_
@@ -16,10 +17,14 @@
 #include <string.h>
 #endif
 
+#ifndef _UNISTD_H_
+#define _UNISTD_H_
+#include <unistd.h>
+#endif
+
 #define URL_FILE "url.txt\0"//for saving the file of url's order to dispatch
 #define urlPerProcess 5//the max amount of url expected tp dispatch to per child process
 
-#define PATH_MAX 0x1000
 #define OUTPUT_DIR "webpage/"
 
 #include "errDefine.h"
